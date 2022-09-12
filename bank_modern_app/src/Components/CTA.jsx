4c2fcc323ccfd60d1b2c864ead2fea6b69264b8d
Akay999 <akaygamer199@gmail.com>
2022-09-13 02:50:09 +0530

    completed CTA

--- bank_modern_app/src/Components/CTA.jsx
+++ bank_modern_app/src/Components/CTA.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let's Try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>Anywhere you need to accept card payments and grow your business anywhere on the planet</p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:mt-0 mt-10 sm:ml-10 ml-0`}>
+      <Button />
+    </div>
+  </section>   
+)
 
 export default CTA
